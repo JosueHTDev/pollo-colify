757dc380d480037b59fe516133f271281f740e9b
PabloDev <yauripablo70@gmail.com>
2026-07-08 15:08:50 -0500

    log

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+console.log("BACKEND URL:", import.meta.env.VITE_BACKEND_URL);
+console.log("ENV:", import.meta.env);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
